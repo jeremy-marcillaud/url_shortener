51325c4f3617c5dc845d69e7f7172c14abd014f1
Jeremy Marcillaud <jeremy@sherpas.com>
2026-02-13 17:09:54 +0100

    feat(api): bootstrap NestJS application

---
+++ api/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard, minutes } from '@nestjs/throttler';
+import { LinkModule } from './link/link.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'default',
+        ttl: minutes(1),
+        limit: 60,
+      },
+    ]),
+    LinkModule,
+  ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
+})
+export class AppModule {}
---
+++ api/src/main.ts
@@ -0,0 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api', { exclude: [':hash'] });
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ origin: configService.getOrThrow<string>('FRONTEND_URL') });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
